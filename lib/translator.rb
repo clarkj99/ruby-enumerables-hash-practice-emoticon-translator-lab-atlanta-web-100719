24a158a7315d0529979e44a5ee1fbd9c3c9cb6fe
Clark Johnson <clarkandkathy@gmail.com>
2019-09-11 19:15:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon (file_path, emoticon)
   library[:get_emoticon].fetch(emoticon, $sorry_message)
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning  (file_path, emoticon)
+  library = load_library(file_path)
+  library[:get_meaning].fetch(emoticon, $sorry_message)
 end
